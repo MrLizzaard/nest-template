45596fdb2fa7e8ad7b46086a8c7642ccc95fcd40
Do Hyeong Kim <dhk21512@gmail.com>
2023-12-19 16:54:20 +0900

    origin 수정

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4000', 'http://43.202.204.173:4000/'],
-    credentials: true,
+    origin: '*',
   });
 
   // ConfigService를 사용하여 구성 정보를 얻습니다.
