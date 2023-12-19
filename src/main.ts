f4453ff9b5eac088efc1a59185d8b30085b328a3
Do Hyeong Kim <dhk21512@gmail.com>
2023-12-19 17:09:51 +0900

    cors수정

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: true,
+    credentials: true,
+    exposedHeaders: ['Authorization'],
   });
 
   // ConfigService를 사용하여 구성 정보를 얻습니다.
